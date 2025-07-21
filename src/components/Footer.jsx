80fbe1d428ea1bcf586f7159a74db52a78967a44
lazerpy <mob4rre@gmail.com>
2025-07-21 12:27:19 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <footer className="w-full bg-gray-900 bg-opacity-80 text-gray-400 text-center py-4 text-sm mt-8">
-    &copy; {new Date().getFullYear()} John Doe. All rights reserved.
+    &copy; {new Date().getFullYear()} Mo barre. All rights reserved.
   </footer>
 );
 
